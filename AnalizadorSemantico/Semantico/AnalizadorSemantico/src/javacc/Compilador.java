f4e4f146470c3356faaeeaccf2a008fd389b6364
ritrix <ritrix@e9f3d2c6-08b7-e33c-422a-1893127c2035>
2013-05-08 20:01:12 +0000

    Investigando cosas del instanceof

--- AnalizadorSemantico/Semantico/AnalizadorSemantico/src/javacc/Compilador.java
+++ AnalizadorSemantico/Semantico/AnalizadorSemantico/src/javacc/Compilador.java
@@ -1885,19 +1885,44 @@ void op_add_e() :
   }
 
   static final public void expresion_instanceof() throws ParseException {
-  if (usaInterfaz)
+ /*@bgen(jjtree) expresion_instanceof */
+  Nodoexpresion_instanceof jjtn000 = new Nodoexpresion_instanceof(JJTEXPRESION_INSTANCEOF);
+  boolean jjtc000 = true;
+  jjtree.openNodeScope(jjtn000);
+  jjtn000.jjtSetFirstToken(getToken(1));if (usaInterfaz)
   {
     InterfazPlg.escribirAvisos();
   }
-    expresion_relacional();
-    switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
-    case INSTANCEOF:
-      jj_consume_token(INSTANCEOF);
-      tipo();
-      break;
-    default:
-      jj_la1[54] = jj_gen;
-      ;
+    try {
+      expresion_relacional();
+      switch ((jj_ntk==-1)?jj_ntk():jj_ntk) {
+      case INSTANCEOF:
+        jj_consume_token(INSTANCEOF);
+        tipo();
+        break;
+      default:
+        jj_la1[54] = jj_gen;
+        ;
+      }
+    } catch (Throwable jjte000) {
+    if (jjtc000) {
+      jjtree.clearNodeScope(jjtn000);
+      jjtc000 = false;
+    } else {
+      jjtree.popNode();
+    }
+    if (jjte000 instanceof RuntimeException) {
+      {if (true) throw (RuntimeException)jjte000;}
+    }
+    if (jjte000 instanceof ParseException) {
+      {if (true) throw (ParseException)jjte000;}
+    }
+    {if (true) throw (Error)jjte000;}
+    } finally {
+    if (jjtc000) {
+      jjtree.closeNodeScope(jjtn000, true);
+      jjtn000.jjtSetLastToken(getToken(0));
+    }
     }
   }
 
