867ffa9a156705f249701a7a2073a4981da5a8eb
Raquelpm23@gmail.com <Raquelpm23@gmail.com@e9f3d2c6-08b7-e33c-422a-1893127c2035>
2013-05-14 16:08:37 +0000

    Clase simbolo

---
+++ AnalizadorSemantico/Semantico/AnalizadorSemantico/src/javacc/Simbolo.java
@@ -0,0 +1,19 @@
+package javacc;
+
+public class Simbolo {
+	public String lugar;
+	public String codigo;
+	public String verdadera;
+	public String falsa;
+	public String siguiente;
+	public String comienzo;
+	
+	public Simbolo(){
+		lugar = "";
+		codigo = "";
+		verdadera = "";
+		falsa = "";
+		comienzo = "";
+		siguiente = "";
+	}
+}
