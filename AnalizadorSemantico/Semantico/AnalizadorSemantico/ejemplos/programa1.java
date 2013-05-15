3ea6226ebca03f46f4d5d3202731bfefb6a7229d
jesuslopezparkour@gmail.com <jesuslopezparkour@gmail.com@e9f3d2c6-08b7-e33c-422a-1893127c2035>
2013-05-15 22:32:33 +0000

    Instrucciones de asignacion simple completadas. También la funcionalidad de crear el prólogo y el epílogo de métodos y funciones, así como de la clase. Ya se puede ejecutar en la máquina virtual ^^

--- AnalizadorSemantico/Semantico/AnalizadorSemantico/ejemplos/programa1.java
+++ AnalizadorSemantico/Semantico/AnalizadorSemantico/ejemplos/programa1.java
@@ -1,12 +1,21 @@
 public class Prueba {
-	int k, j, h;
-	int b = 100;
-	boolean a;
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
 	
 	void main(){
-		int i;
-		i = 0;
-		k = i++;
-		//b = i+k*3;
+		int f, g, h, i, j;
+		f = a;
+		g = b;
+		h = c;
+		i = d;
+		j = e;
+		a = 6;
+		b = 7;
+		c = 8;
+		d = 9;
+		e = 10;
 	}
 }
